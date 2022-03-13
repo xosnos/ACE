d75f523318eaa217df9196413429ce47a8ee9b8b
Steven Nguyen <steyen@umich.edu>
2022-03-13 15:59:23 -0400

    swiftUI project

---
+++ swiftRANGR/swiftRANGR/swiftRANGRApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftRANGRApp.swift
+//  swiftRANGR
+//
+//  Created by Steven Nguyen on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftRANGRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
